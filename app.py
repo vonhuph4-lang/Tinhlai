ea640c371ac5e719a01632e285b7e051bbcb1b52
vonhuph4-lang <vonhuph4@gmail.com>
2026-06-17 14:11:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.image("IMG_20260213_195713_641")
+st.image("IMG_20260213_195713_641.jpg")
 # Tiêu đề app
 st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM_ĐỀ TÀI 2_TS. VŨ ĐỨC BÌNH")
 
